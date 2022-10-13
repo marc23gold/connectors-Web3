3ef822e645b0a5c009414ece83b2ef02e359f470
Marcx <haitianhero6@gmail.com>
2022-10-13 12:50:13 -0400

    ok

--- nextjs-ethers/pages/index.js
+++ nextjs-ethers/pages/index.js
@@ -8,7 +8,7 @@ import { ethers } from "ethers";
 
 export default function Home() {
   const [isConnected, setIsConnected] = useState(false);
-  const [provider, setProvider] = useState();
+  const [signer, setSigner] = useState();
 
   async function connect() {
     if (typeof window.ethereum !== "undefined") {
@@ -25,11 +25,120 @@ export default function Home() {
     } else {
       setIsConnected(false);
     }
+
+    async function execute() {
+      if (typeof window.ethereum !== "undefined") {
+        //address
+        const contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+        //contract ABI
+        const abi = [
+          {
+            inputs: [
+              {
+                internalType: "string",
+                name: "_name",
+                type: "string",
+              },
+              {
+                internalType: "uint256",
+                name: "_favoriteNumber",
+                type: "uint256",
+              },
+            ],
+            name: "addPerson",
+            outputs: [],
+            stateMutability: "nonpayable",
+            type: "function",
+          },
+          {
+            inputs: [
+              {
+                internalType: "string",
+                name: "",
+                type: "string",
+              },
+            ],
+            name: "nameToFavoriteNumber",
+            outputs: [
+              {
+                internalType: "uint256",
+                name: "",
+                type: "uint256",
+              },
+            ],
+            stateMutability: "view",
+            type: "function",
+          },
+          {
+            inputs: [
+              {
+                internalType: "uint256",
+                name: "",
+                type: "uint256",
+              },
+            ],
+            name: "people",
+            outputs: [
+              {
+                internalType: "uint256",
+                name: "favoriteNumber",
+                type: "uint256",
+              },
+              {
+                internalType: "string",
+                name: "name",
+                type: "string",
+              },
+            ],
+            stateMutability: "view",
+            type: "function",
+          },
+          {
+            inputs: [],
+            name: "retrieve",
+            outputs: [
+              {
+                internalType: "uint256",
+                name: "",
+                type: "uint256",
+              },
+            ],
+            stateMutability: "view",
+            type: "function",
+          },
+          {
+            inputs: [
+              {
+                internalType: "uint256",
+                name: "_favoriteNumber",
+                type: "uint256",
+              },
+            ],
+            name: "store",
+            outputs: [],
+            stateMutability: "nonpayable",
+            type: "function",
+          },
+        ];
+        //function
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
+        const signer = provider.getSigner(); //will get the connected account
+        const contract = new ethers.Contract(contractAddress, abi, signer);
+        await contract.store(42);
+      }
+    }
   }
   return (
     <div className={styles.container}>
       Hello Patrick
-      <button onClick={() => connect()}>Connect </button>
+      {isConnected ? (
+        <>
+          "Connected"
+          <button onClick={() => execute()}>Execute</button>
+        </>
+      ) : (
+        <button onClick={() => connect()}>Connect! </button>
+      )}
     </div>
   );
 }
